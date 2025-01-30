55979ef6321c158d3ed4a7bbaabb805353d8ca11
itsmohammedumar <155177000+itsmohammedumar@users.noreply.github.com>
2025-01-30 06:02:17 -0800

    Initial commit

---
+++ wether app.cs
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wethar</title>
+    <img src="c:\Users\User\Desktop\Umar html\New folder\wther.jpg">
+    <link rel="stylesheet" href=css/style.css>
+</head>
+
+<body>
+    <div class="wether">
+        <input type="search"class="wether___search" placeholder="your city">
+        <div class="wether___today">
+    </div>
+    <div class="wether___today">
+    <img src="c:\Users\User\Desktop\Umar html\New folder\wther.jpg">
+</head>   
+</body>
+</html>
